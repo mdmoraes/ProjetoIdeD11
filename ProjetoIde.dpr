92dfb1e51f2be77321319e7852f214af3a6785d1
mdmoraes.mopsus@gmail.com <mdmoraes>
2023-12-02 15:26:15 -0300

    FirstCommit

---
+++ ProjetoIde.dpr
@@ -0,0 +1,14 @@
+program ProjetoIde;
+
+uses
+  Vcl.Forms,
+  UPrincipal in 'UPrincipal.pas' {Form19};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm19, Form19);
+  Application.Run;
+end.
